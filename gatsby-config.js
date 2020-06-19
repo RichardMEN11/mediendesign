7f574833f1ac777edc3b4415a8f0b6d3a55d3388
Richard <richardmenning@googlemail.com>
2020-06-19 11:56:48 +0200

    remove source wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,67 +31,6 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     {
-      resolve: "gatsby-source-wordpress",
-      options: {
-        /*
-         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
-         * Example : 'demo.wp-api.org' or 'www.example-site.com'
-         */
-        baseUrl: "http://wp22.wplab01.hs-rw.de",
-        protocol: "http",
-        // The rest api route prefix that your WordPress site is using.
-        // Sometimes this is modified by WordPress plugins.
-        // If not set, it uses the default of "wp-json"
-        restApiRoutePrefix: "wp-json",
-        hostingWPCOM: false,
-        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
-        // This feature is untested for sites hosted on wordpress.com.
-        // Defaults to true.
-        useACF: true,
-        // Include specific ACF Option Pages that have a set post ID
-        // Regardless if an ID is set, the default options route will still be retrieved
-        // Must be using V3 of ACF to REST to include these routes
-        // Example: `["option_page_1", "option_page_2"]` will include the proper ACF option
-        // routes with the ID option_page_1 and option_page_2
-        // The IDs provided to this array should correspond to the `post_id` value when defining your
-        // options page using the provided `acf_add_options_page` method, in your WordPress setup
-        // Dashes in IDs will be converted to underscores for use in GraphQL
-        acfOptionPageIds: [],
-        // Set cookies that should be send with requests to WordPress as key value pairs
-        cookies: {},
-        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
-        // It can help you debug specific API Endpoints problems.
-        verboseOutput: false,
-        // Set how many pages are retrieved per API request.
-        perPage: 100,
-        // Set how many simultaneous requests are sent at once.
-        concurrentRequests: 10,
-        // Set WP REST API routes whitelists
-        // and blacklists using glob patterns.
-        // Defaults to whitelist the routes shown
-        // in the example below.
-        // See: https://github.com/isaacs/minimatch
-        // Example:  `["/*/*/comments", "/yoast/**"]`
-        // ` will either include or exclude routes ending in `comments` and
-        // all routes that begin with `yoast` from fetch.
-        // Whitelisted routes using glob patterns
-        includedRoutes: [
-          "**/categories",
-          "**/posts",
-          "**/pages",
-          "**/media",
-          "**/tags",
-          "**/taxonomies",
-          "**/users",
-        ],
-        // Set this to keep media sizes.
-        // This option is particularly useful in case you need access to
-        // URLs for thumbnails, or any other media detail.
-        // Defaults to false
-        keepMediaSizes: false,
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
